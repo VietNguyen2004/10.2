95496a30df6017742b8c047443411f4e809c8c91
VietNguyen2004 <85068521+VietNguyen2004@users.noreply.github.com>
2021-08-17 22:26:17 +0700

    Create Bai3.cpp

---
+++ Bai3.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+#define ll long long
+
+ll solve(ll a,ll b)
+{
+	while(b != 0)
+	{
+		swap(a,b);
+		b %= a;
+	}
+	return a;
+}
+
+int main()
+{
+	ll a,b;
+	cin >> a >> b;
+	cout << solve(a,b);
+	return 0;
+}
