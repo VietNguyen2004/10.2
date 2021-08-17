fbceeaf17bf08b51a8e3f4e3979800a2320457f4
VietNguyen2004 <85068521+VietNguyen2004@users.noreply.github.com>
2021-08-18 06:53:24 +0700

    Create Bai6.cpp

---
+++ Bai6.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+#define ll long long234
+
+ll gcd(ll c, ll d)
+{
+	while(d != 0)
+	{
+		swap(c,d);
+		d %= c;
+	}
+	return c;
+}
+ll lcm(ll c, ll d)
+{
+	return c * d / gcd(c,d);
+}
+
+ll solve(ll a,ll b,ll c)
+{
+	ll start = (a % c ==0) ? a/c : a/c + 1;
+	ll ends = b/c;
+	return (ends-start) + 1;
+}
+
+int main()
+{
+	ll a,b,c,d;
+	cin >> a >> b >> c >> d;
+	cout<<solve(a,b,c) + solve(a,b,d) - solve(a,b,lcm(c,d));
+}
